25244e6cd81d33cdebd1c6acaedefd20cb42c01c
양찬규 <yangchan00@ship-da.com>
2023-02-27 19:57:01 +0900

    add reflect apply

--- 23_Javascript_Reflect/main.ts
+++ 23_Javascript_Reflect/main.ts
@@ -1,3 +1,17 @@
 (function () {
   console.log("hello");
+  apply();
 })();
+
+function apply() {
+  Reflect.apply(
+    (a: any, b: any, c: any) => {
+      console.log(a, b, c);
+    },
+    undefined,
+    [1, 2, 3]
+  );
+
+  console.log(Reflect.apply("".charAt, "abcdef", [3]));
+  console.log(Reflect.apply(String.prototype.trim, " aaabbb ", []));
+}
